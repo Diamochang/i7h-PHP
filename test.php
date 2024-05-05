78534434676ecfcdb74e342b9ea30f7bdae36aac
Diamochang <diamochang@yeah.net>
2024-05-05 11:32:40 +0800

    Test Code

--- test.php
+++ test.php
@@ -1,5 +1,7 @@
 <?php
 /** 用于测试代码。For testing. */
+/** Correct output: H3o W3d, K8s, i18n a1d l10n! */
+
 require_once "i7h.php";
 $i7h = new i7h();
 echo $i7h->i18n('Hello World, Kubernetes, internationalization and localization!')
